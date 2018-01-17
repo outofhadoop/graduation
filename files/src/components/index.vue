5cfc621981a1f4cf4f7982af6b45300ce4a999d7
outofhadoop <tianyu.han@aliyun.com>
2018-01-17 23:57:10 +0800

    index

---
+++ files/src/components/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="index">
+    <top-nav></top-nav>
+  </div>
+</template>
+
+<script>
+import indexTopNav from '@/components/topBar/indexTopBar'
+export default {
+  name: 'index',
+  data () {
+    return {
+      
+    }
+  },
+  components: {
+    'top-nav': indexTopNav
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ files/src/components/topBar/indexTopBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="indexNavBox">
+      <header>
+          <div>
+              <a href="" class="navLogo"></a>
+          </div>
+      </header>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+      return {
+
+      }
+  }
+}
+</script>
+<style lang="css" scoped>
+
+</style>
